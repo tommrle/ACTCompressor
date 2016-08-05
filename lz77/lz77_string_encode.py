17d626b288e8b34ee54445ad1a12e0e76b7b4f48
TMorreale0 <ThomasMorreale@Gmail.com>
2016-08-04 21:46:25 -0500

    upgraded lz77 to use in memory sliding window to calculate repition

--- lz77/lz77_string_encode.py
+++ lz77/lz77_string_encode.py
@@ -25,3 +25,32 @@ def lz77_process_string(data):
         # print "============="
     return data
 
+
+def lz77_process_string_classic(data, window_size=256, min_length=3, max_length=17):
+    index = 0
+    compressed_data = data
+    compressed_data_index = 0
+    while index + min_length < len(data):
+        window_start = index - window_size
+        if window_start < 0:
+            window_start = 0
+
+        while window_start + min_length < index:
+            if data[window_start:window_start+min_length] == data[index:index+min_length]:
+                length = min_length
+                while index+length < len(data) and data[window_start+length] == data[index+length] and length < max_length:
+                    length += 1
+                new_abbreviation = "["
+                new_abbreviation += str(index-window_start)
+                new_abbreviation += ","
+                new_abbreviation += str(length)
+                new_abbreviation += "]"
+                compressed_data = compressed_data[:compressed_data_index] + new_abbreviation + compressed_data[compressed_data_index+length:]
+                index += length
+                compressed_data_index += len(new_abbreviation)
+                break
+
+            window_start += 1
+        index += 1
+        compressed_data_index += 1
+    return compressed_data
